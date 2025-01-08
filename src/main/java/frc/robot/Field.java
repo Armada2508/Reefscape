b6109fc5a6fc89410b1ba7119edbbb5c31d40d09
chris <christopp1228@gmail.com>
2025-01-08 09:07:09 -0600

    station and processor constants

--- src/main/java/frc/robot/Field.java
+++ src/main/java/frc/robot/Field.java
@@ -8,8 +8,9 @@ import edu.wpi.first.units.measure.Distance;
 
 public class Field {
     //TODO double check all values in at some point, good chance something could be wrong here, rookie task?
-    // X is left and right from origin, Y is top to bottom
-    // Blue is origin
+    //^  X is left and right from origin, Y is top to bottom
+
+    // Blue bottom corner is the origin
     public static final Pose2d origin = new Pose2d();
 
     // Field
@@ -17,31 +18,34 @@ public class Field {
     public static final Distance fieldWidth = Inches.of(317);
 
     // Reef
-    public static final Translation2d blueReefA = new Translation2d();
-    public static final Translation2d blueReefB = new Translation2d();
-    public static final Translation2d blueReefC = new Translation2d();
-    public static final Translation2d blueReefD = new Translation2d();
-    public static final Translation2d blueReefE = new Translation2d();
-    public static final Translation2d blueReefF = new Translation2d();
-    public static final Translation2d blueReefG = new Translation2d();
-    public static final Translation2d blueReefH = new Translation2d();
-    public static final Translation2d blueReefI = new Translation2d();
-    public static final Translation2d blueReefJ = new Translation2d();
-    public static final Translation2d blueReefK = new Translation2d();
-    public static final Translation2d blueReefL = new Translation2d();
+    //cycled from A - K - I - G - E - C 
+    //            B - L - J - H - F - D 
 
-    public static final Translation2d redReefA = new Translation2d();
-    public static final Translation2d redReefB = new Translation2d();
-    public static final Translation2d redReefC = new Translation2d();
-    public static final Translation2d redReefD = new Translation2d();
-    public static final Translation2d redReefE = new Translation2d();
-    public static final Translation2d redReefF = new Translation2d();
-    public static final Translation2d redReefG = new Translation2d();
-    public static final Translation2d redReefH = new Translation2d();
-    public static final Translation2d redReefI = new Translation2d();
-    public static final Translation2d redReefJ = new Translation2d();
-    public static final Translation2d redReefK = new Translation2d();
-    public static final Translation2d redReefL = new Translation2d();
+    public static final Pose2d blueReefA = new Pose2d();
+    public static final Pose2d blueReefB = new Pose2d();
+    public static final Pose2d blueReefC = new Pose2d();
+    public static final Pose2d blueReefD = new Pose2d();
+    public static final Pose2d blueReefE = new Pose2d();
+    public static final Pose2d blueReefF = new Pose2d();
+    public static final Pose2d blueReefG = new Pose2d();
+    public static final Pose2d blueReefH = new Pose2d();
+    public static final Pose2d blueReefI = new Pose2d();
+    public static final Pose2d blueReefJ = new Pose2d();
+    public static final Pose2d blueReefK = new Pose2d();
+    public static final Pose2d blueReefL = new Pose2d();
+
+    public static final Pose2d redReefA = new Pose2d();
+    public static final Pose2d redReefB = new Pose2d();
+    public static final Pose2d redReefC = new Pose2d();
+    public static final Pose2d redReefD = new Pose2d();
+    public static final Pose2d redReefE = new Pose2d();
+    public static final Pose2d redReefF = new Pose2d();
+    public static final Pose2d redReefG = new Pose2d();
+    public static final Pose2d redReefH = new Pose2d();
+    public static final Pose2d redReefI = new Pose2d();
+    public static final Pose2d redReefJ = new Pose2d();
+    public static final Pose2d redReefK = new Pose2d();
+    public static final Pose2d redReefL = new Pose2d();
 
     // Barge
     public static final Translation2d blueCageTop = new Translation2d(Inches.of(345.4375), Inches.of(285.875));
@@ -53,13 +57,16 @@ public class Field {
     public static final Translation2d redCageLow = new Translation2d(Inches.of(345.4375), fieldWidth.minus(blueCageLow.getMeasureY()));
 
     // Coral Station
-    public static final Translation2d blueStationTop = new Translation2d();
     public static final Translation2d blueStationLow = new Translation2d(Inches.of(130.71), Inches.of(81.69));
+    public static final Translation2d blueStationTop = new Translation2d(blueStationLow.getMeasureX(), fieldWidth.minus(blueStationLow.getMeasureY()));
 
-    public static final Translation2d redStationTop = new Translation2d();
-    public static final Translation2d redStationLow = new Translation2d();
+    public static final Translation2d redStationLow = new Translation2d(fieldLength.minus(blueStationLow.getMeasureX()), blueStationLow.getMeasureY());
+    public static final Translation2d redStationTop = new Translation2d(fieldLength.minus(blueStationLow.getMeasureX()), fieldWidth.minus(blueStationLow.getMeasureY()));
 
     // Processor
+    //^ processor location is on the edge of the arena carpet rather then the exact middle of the structure
+    public static final Translation2d blueProcessor = new Translation2d(Inches.of(0), Inches.of(235.7255));
 
+    public static final Translation2d redProcessor = new Translation2d(fieldWidth, fieldLength.minus(blueProcessor.getMeasureY()));
     
 }
