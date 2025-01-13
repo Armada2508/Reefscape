2bb94f5b01939672eca5eee345346b407d4d6cd9
chris <christopp1228@gmail.com>
2025-01-13 15:20:34 -0600

    reef constants

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,13 +16,16 @@ public class Robot extends TimedRobot {
     
     public Robot() {
         SmartDashboard.putData("Arena: ", reefTest);
-        reefTest.getObject("Branch K").setPose(Field.blueReefK);
-        
-        reefTest.getObject("Branch A").setPose(Field.blueReefA);
-        reefTest.getObject("Branch I").setPose(Field.blueReefI);
-        reefTest.getObject("Branch G").setPose(Field.blueReefG);
-        reefTest.getObject("Branch E").setPose(Field.blueReefE);
-        reefTest.getObject("Branch C").setPose(Field.blueReefC);
+
+        //cycled clockwise A - K - I - G - E - C 
+    //                     B - L - J - H - F - D 
+
+        reefTest.getObject("Branch B").setPose(Field.blueReefB);
+        reefTest.getObject("Branch L").setPose(Field.blueReefL);
+        reefTest.getObject("Branch J").setPose(Field.blueReefJ);
+        reefTest.getObject("Branch H").setPose(Field.blueReefH);
+        reefTest.getObject("Branch F").setPose(Field.blueReefF);
+        reefTest.getObject("Branch D").setPose(Field.blueReefD);
         
         reefTest.getObject("Origin").setPose(Field.origin);
         reefTest.getObject("Blue Center").setPose(Field.blueReefCenter.getX(), Field.blueReefCenter.getY(), new Rotation2d(0));
