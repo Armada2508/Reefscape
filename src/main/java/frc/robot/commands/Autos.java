aa43085678075f07954f7e4953fd3704e242f4f8
Wispy Sparks <wispysparks@gmail.com>
2025-01-15 15:45:50 -0600

    Add commands classes

---
+++ src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,8 @@
+package frc.robot.commands;
+
+public class Autos {
+
+    // Prevent this class from being instantiated
+    private Autos() {}
+    
+}
---
+++ src/main/java/frc/robot/commands/Routines.java
@@ -0,0 +1,8 @@
+package frc.robot.commands;
+
+public class Routines {
+
+    // Prevent this class from being instantiated
+    private Routines() {}
+    
+}
