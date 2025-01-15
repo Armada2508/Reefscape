f3778ef3758f272c1be636e3f95e229eb96b1964
Wispy Sparks <wispysparks@gmail.com>
2025-01-15 15:41:08 -0600

    new robot design

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,6 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Climb extends SubsystemBase {
+public class Intake extends SubsystemBase {
     
 }
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
-public class Vision {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
     
 }
