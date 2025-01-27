f80ed5a9075dddab53a44a058bb5d0c10bd2b91d
Andrew <baumgand000@stillwaterschools.org>
2025-01-27 16:37:57 -0600

    Improved functions

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,34 +1,35 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import java.util.HashMap;
+
+import edu.wpi.first.units.measure.LinearAcceleration;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimbK;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.StrictFollower;
 
-import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.LinearAcceleration;
-import edu.wpi.first.units.measure.LinearVelocity;
-import edu.wpi.first.units.measure.Voltage;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ElevatorK;
-import frc.robot.lib.util.Encoder;
 import frc.robot.lib.util.Util;
 public class Climb extends SubsystemBase {
+    
     TalonFX armMotor = new TalonFX(ClimbK.rMotorID);
     TalonFX armMotorFollow = new TalonFX(ClimbK.frMotorID);
+
+    LinearVelocity linearVelocity = ClimbK.velocity;
+    LinearAcceleration linearAcceleration = ClimbK.acceleration;
     //^Takes the number from Constant's ClimbK class and uses it as the motor ID^
     //Configure motors
-    private void configureTalons() {
+    public Climb() {
+        configTalons();
+        configMotionMagic(linearVelocity,linearAcceleration);
+    }
+    
+    
+    
+    private void configTalons() {
         
         
         
@@ -37,8 +38,9 @@ public class Climb extends SubsystemBase {
         
         armMotorFollow.setControl(new StrictFollower(ClimbK.rMotorID));
     }
+    
     //MotionMagic
-    private void MotionMagicConfig() {
+    private void configMotionMagic(LinearVelocity velocity, LinearAcceleration acceleration) {
         //Set the Configs
         MotionMagicConfigs MotionMagicConfigs = new MotionMagicConfigs();
         MotionMagicConfigs.MotionMagicCruiseVelocity = 0; //Find
@@ -52,15 +54,15 @@ public class Climb extends SubsystemBase {
     //     armMotor.setControl(request);
     
     //Climb
-    public void deepclimb() {
+    public Command deepclimb() {
         
         
         //Simply need to rotate arms until they need to stop.
         //Not sure if there will be a sensor that will be there or if it'll have to be a set degrees.
-        configureTalons();
+        
         // setVoltage(ClimbK.voltage);
         MotionMagicVoltage request = new MotionMagicVoltage(ClimbK.climbArmDown);
-
+        return runOnce(() -> armMotor.setControl(request));
 
 
     }
