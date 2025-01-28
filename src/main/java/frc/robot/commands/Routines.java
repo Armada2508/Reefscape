4289ea1cadd9d1ce01228f2ef63359b426f4ae1c
Audrey <davisaud000@stillwaterschools.org>
2025-01-28 15:53:34 -0600

    Stow elevator + intake and scoring commands done

--- src/main/java/frc/robot/commands/Routines.java
+++ src/main/java/frc/robot/commands/Routines.java
@@ -1,7 +1,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ElevatorK.Positions;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
 
 public class Routines {
 
@@ -9,6 +11,36 @@ public class Routines {
     private Routines() {}
     
     public static Command stowElevator(Elevator elevator) {
-        return; //! fix
+        return elevator.setPosition(Positions.STOW);
+    }
+
+    public static Command intakeCoral(Elevator elevator, Intake intake) {
+        return elevator.setPosition(Positions.INTAKE)
+        .andThen(intake.coralIntake())
+        .finallyDo(() -> elevator.setPosition(Positions.STOW));
+    }
+
+    public static Command scoreCoralLevelOne(Elevator elevator, Intake intake) {
+        return elevator.setPosition(Positions.L1)
+        .andThen(intake.scoreLevelOne())
+        .finallyDo(() -> elevator.setPosition(Positions.STOW));
+    }
+
+    public static Command scoreCoralLevelTwo(Elevator elevator, Intake intake) {
+        return elevator.setPosition(Positions.L2)
+        .andThen(intake.scoreLevelTwoThree())
+        .finallyDo(() -> elevator.setPosition(Positions.STOW));
+    }
+
+    public static Command scoreCoralLevelThree(Elevator elevator, Intake intake) {
+        return elevator.setPosition(Positions.L3)
+        .andThen(intake.scoreLevelTwoThree())
+        .finallyDo(() -> elevator.setPosition(Positions.STOW));
+    }
+    
+    public static Command scoreCoralLevelFour(Elevator elevator, Intake intake) {
+        return elevator.setPosition(Positions.L4)
+        .andThen(intake.scoreLevelFour())
+        .finallyDo(() -> elevator.setPosition(Positions.STOW));
     }
 }
