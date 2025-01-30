710ba9e1497815eb856239a99ddf2460762d34de
Audrey <davisaud000@stillwaterschools.org>
2025-01-30 16:18:05 -0600

    Algae removal (high and low) and coral station alignment added to routines

--- src/main/java/frc/robot/commands/Routines.java
+++ src/main/java/frc/robot/commands/Routines.java
@@ -1,9 +1,12 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ElevatorK.Positions;
+import frc.robot.subsystems.Algae;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Swerve;
 
 public class Routines {
 
@@ -42,5 +45,27 @@ public class Routines {
         return elevator.setPosition(Positions.L4)
         .andThen(intake.scoreLevelFour())
         .finallyDo(() -> elevator.setPosition(Positions.STOW));
+    }
+
+    public static Command removeAlgaeLow(Elevator elevator, Algae algae, Swerve swerve) {
+        return elevator.setPosition(Positions.ALGAE_LOW)
+        .andThen(algae.loweredPosition())
+        .andThen(algae.algaePosition())
+        .andThen(swerve.driveToPoseCommand(new Pose2d())) //!make it move backwards
+        .andThen(algae.stow())
+        .finallyDo(() -> elevator.setPosition(Positions.STOW));
+    }
+
+    public static Command removeAlgaeHigh(Elevator elevator, Algae algae, Swerve swerve) {
+        return elevator.setPosition(Positions.ALGAE_HIGH)
+        .andThen(algae.loweredPosition())
+        .andThen(algae.algaePosition())
+        .andThen(swerve.driveToPoseCommand(new Pose2d())) //!make it move backwards
+        .andThen(algae.stow())
+        .finallyDo(() -> elevator.setPosition(Positions.STOW));
+    }
+
+    public static Command alignCoralStation(Swerve swerve) {
+        return swerve.alignToCoralStation();
     }
 }
