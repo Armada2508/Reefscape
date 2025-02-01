cb77a5d55599307ccb153445503b296140108d01
Andrew <baumgand000@stillwaterschools.org>
2025-02-01 14:44:55 -0600

    added zeroing to the release (zero) command

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,17 +1,5 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-
-
-import edu.wpi.first.units.measure.AngularAcceleration;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Voltage;
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ClimbK;
-
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 
@@ -20,17 +8,27 @@ import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ReverseLimitValue;
 
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimbK;
 import frc.robot.lib.util.Util;
 public class Climb extends SubsystemBase {
 
     private final TalonFX armMotor = new TalonFX(ClimbK.armMotorID);
     private final TalonFX armMotorFollow = new TalonFX(ClimbK.fArmMotorID);
-    //^Takes the number from Constant's ClimbK class and uses it as the motor ID^
+    public boolean isZeroed = false;
+    //↑Takes the number from Constant's ClimbK class and uses it as the motor ID↑
     //Configure motors
     public Climb() {
-        configTalons();
         configMotionMagic(ClimbK.velocity, ClimbK.acceleration); 
+        configTalons();
     } 
     private void configTalons() {
         Util.factoryReset(armMotor, armMotorFollow);
@@ -50,41 +48,50 @@ public class Climb extends SubsystemBase {
         armMotor.getConfigurator().apply(MotionMagicConfigs);
     }
     /**
-     * Sets the volts of the motors
-     * @param volts speed of the motor in volts
+     * Sets the volts of the motors.
+     * @param volts speed of the motor in volts.
      */
     public Command setVoltage(Voltage volts) {
         VoltageOut request = new VoltageOut(volts);
         return runOnce(() -> armMotor.setControl(request))
         .withName("Set Voltage");
     }
+    
     /**
      * Command for activating the climbing arms, suspending the robot in midair via use of a cage.
      * 
      */
-    public Command deepclimb() {
-        return setVoltage(ClimbK.climbVoltage)
+     public Command deepclimb() {
+        // onTrue
+        return Commands.either(setVoltage(ClimbK.climbVoltage)
+        .andThen(Commands.waitUntil(() -> armMotor.getPosition().getValue().isNear(ClimbK.maxAngle, ClimbK.allowableError)))
         .withName("Climbed")
-        .andThen(Commands.waitUntil(() -> armMotor.getPosition().getValue().isNear(ClimbK.maxAngle, ClimbK.allowableError)));
+        // onFalse
+        ,Commands.none()
+        // selector
+        , () -> isZeroed);
     }
     /**
      * Motion magic version of the deepClimb command.
      * 
      */
     public Command deepClimbMotionMagic() {
+        
         MotionMagicVoltage request = new MotionMagicVoltage(ClimbK.maxAngle);
         return runOnce(() -> armMotor.setControl(request))
         .andThen(Commands.waitUntil(() -> armMotor.getPosition().getValue().isNear(ClimbK.maxAngle, ClimbK.allowableError)))
-        .withName("Climbed with Motion Magic");
+        .withName("Climbed Motion Magic");
     }
     /** 
      * Command for bringing the arms back up, setting the robot (in a climbing state) back on the ground
      * 
      */
-    public Command release() {
+    public Command zero() {
         return setVoltage(ClimbK.climbVoltage.unaryMinus())
+        .andThen(Commands.waitUntil(() -> armMotor.getPosition().getValue().isNear(ClimbK.minAngle, ClimbK.allowableError)))
         .withName("Released")
-        .andThen(Commands.waitUntil(() -> armMotor.getPosition().getValue().isNear(ClimbK.minAngle, ClimbK.allowableError)));
+        .andThen(Commands.waitUntil(() -> armMotor.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround))
+        .andThen(() -> {isZeroed = true;});
 
     }
     /** Motion magic version of the Release command.
