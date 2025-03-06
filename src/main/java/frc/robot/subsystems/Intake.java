5fc9eb2761dfdec7aa9e78ca71e8340a29e3cea1
Wispy Sparks <wispysparks@gmail.com>
2025-03-06 15:13:22 -0600

    Remove testing code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -34,17 +34,6 @@ public class Intake extends SubsystemBase {
     public Intake() {
         configSparkMax();
         timeOfFlight.setRangingMode(RangingMode.Short, 24);
-        setDefaultCommand(run(() -> { // TODO: Finalize + move into another branch
-            if (isSensorTripped()) {
-                // var volts = Volts.of(v.getAsDouble());
-                // System.out.println("Do stuff " + volts);
-                // sparkMaxLeft.setVoltage(volts);
-                // sparkMaxRight.setVoltage(volts);
-            }
-            else {
-                stop();
-            }
-        }).withName("Hold Coral"));
     }
 
     private void configSparkMax() {
