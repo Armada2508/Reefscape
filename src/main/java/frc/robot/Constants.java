7dd94a3d43d8293927c0c52ad81d250a10da99a2
chris <christopp1228@gmail.com>
2025-03-18 17:48:10 -0500

    tof offset

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -187,7 +187,7 @@ public class Constants {
         }
 
         // Linear Interpolation
-        public static final Distance timeOfFlightOffset = Inches.of(-13.8);
+        public static final Distance timeOfFlightOffset = Inches.of(-13);
         public static final Distance maxLinearDistance = Inches.of(4.5);
         public static final int averageSamples = 25;
     }  
