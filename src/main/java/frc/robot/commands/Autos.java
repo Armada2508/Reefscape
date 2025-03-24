8a2782d62b6cf5cb470248cbb66f6a343332e74e
chris <christopp1228@gmail.com>
2025-03-24 15:53:31 -0500

    stow at start of paths, score as named command

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -23,20 +23,22 @@ public class Autos {
     public static SendableChooser<Command> initPathPlanner(Swerve swerve, Elevator elevator, Intake intake) {
         FollowPathCommand.warmupCommand().schedule();
 
-        NamedCommands.registerCommand("intake named", Routines.intakeCoral(elevator, intake));
-        NamedCommands.registerCommand("score L1", Routines.scoreCoralLevelOne(elevator, intake));
-        NamedCommands.registerCommand("score L2", Routines.scoreCoralLevelTwo(elevator, intake));
-        NamedCommands.registerCommand("score L3", Routines.scoreCoralLevelThree(elevator, intake));
-        NamedCommands.registerCommand("raise and score L4", elevator.setPositionCommand(Positions.L4.close).andThen(Routines.scoreCoralLevelFour(elevator, intake)));
-        NamedCommands.registerCommand("wait for score", Commands.waitUntil(() -> !intake.isSensorTripped()).withName("Wait for score"));
+        // NamedCommands.registerCommand("intake named", Routines.intakeCoral(elevator, intake));
+        // NamedCommands.registerCommand("score L1", Routines.scoreCoralLevelOne(elevator, intake));
+        // NamedCommands.registerCommand("score L2", Routines.scoreCoralLevelTwo(elevator, intake));
+        // NamedCommands.registerCommand("score L3", Routines.scoreCoralLevelThree(elevator, intake));
+        // NamedCommands.registerCommand("raise and score L4", elevator.setPositionCommand(Positions.L4.close).andThen(Routines.scoreCoralLevelFour(elevator, intake)));
+        NamedCommands.registerCommand("score L4", Commands.waitUntil(() -> elevator.nearHeight(Positions.L4.close)).andThen(intake.scoreLevelFour()).withName("Auto score L4").asProxy());
+        
+        // NamedCommands.registerCommand("wait for score", Commands.waitUntil(() -> !intake.isSensorTripped()).withName("Wait for score"));
         NamedCommands.registerCommand("wait for intake", Commands.waitUntil(intake::isSensorTripped).andThen(Commands.waitTime(IntakeK.intakeAfterTrip)));
-        NamedCommands.registerCommand("drive to coral", swerve.driveCommand(() -> 0.2, () -> 0, () -> 0, false, true).until(() -> elevator.getTimeOfFlightDistance() < 14).withTimeout(2).finallyDo(swerve::stop));
-
+        // NamedCommands.registerCommand("drive to coral", swerve.driveCommand(() -> 0.2, () -> 0, () -> 0, false, true).until(() -> elevator.getTimeOfFlightDistance() < 14).withTimeout(2).finallyDo(swerve::stop));
+        
         new EventTrigger("intake coral").onTrue(Routines.intakeCoral(elevator, intake));
-        new EventTrigger("score L4").onTrue(Commands.waitUntil(() -> elevator.nearHeight(Positions.L4.close)).andThen(Routines.scoreCoralLevelFour(elevator, intake)).withName("Auto score L4"));
         new EventTrigger("raise elevator to intake").onTrue(elevator.setPositionCommand(Positions.INTAKE.close));
         new EventTrigger("raise elevator to L4").onTrue(elevator.setPositionCommand(Positions.L4).alongWith(intake.coralIntake()));
         new EventTrigger("stow elevator").onTrue(elevator.setPositionCommand(Positions.STOW));
+        
         new EventTrigger("test").onTrue(Commands.print("test event trigger"));
 
         SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser("Low Score 2 Coral Top");
