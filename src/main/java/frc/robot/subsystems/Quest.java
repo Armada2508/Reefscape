52a4f85f254af95c1304e3ddc6c602665d901270
FarmerOfEggplants <andrewboy1389@gmail.com>
2025-12-23 16:22:27 -0600

    added @Logged to qutestnav imports

--- src/main/java/frc/robot/subsystems/Quest.java
+++ src/main/java/frc/robot/subsystems/Quest.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.Utils;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose3d;
@@ -13,6 +14,7 @@ import gg.questnav.questnav.PoseFrame;
 import gg.questnav.questnav.QuestNav;
 import swervelib.SwerveDrive;
 
+@Logged
 public class Quest extends SubsystemBase {
     QuestNav questNav = new QuestNav();
     
